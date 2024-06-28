4f20219b3d937438c28c0e2b595308deb2fb75da
Shehab Elhariry <cheha6@gmail.com>
2024-06-28 12:41:57 +0300

    feat: added react router and created basic layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,18 @@
+import { Route, Routes } from "react-router-dom";
 import "./App.css";
+import MainLayout from "./layouts/MainLayout";
+import Home from "./pages/Home";
+import ArticleDetails from "./pages/ArticleDetails";
 
 function App() {
   return (
     <>
-      <div>Hello News App</div>
+      <Routes>
+        <Route path="/" element={<MainLayout />}>
+          <Route index element={<Home />} />
+          <Route path="article/:id" element={<ArticleDetails />} />
+        </Route>
+      </Routes>
     </>
   );
 }
